2e1080497db235f7e5ade5912dfb09f6f645a898
AdrianOlivares77 <adrian.olivares.f.52@gmail.com>
2021-12-26 15:14:56 -0300

    feat(fav-news): new component with the favorite news

---
+++ src/app/components/fav-news/fav-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fav-news',
+  templateUrl: './fav-news.component.html',
+  styleUrls: ['./fav-news.component.css']
+})
+export class FavNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
