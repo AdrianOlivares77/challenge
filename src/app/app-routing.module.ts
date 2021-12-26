9dc79c10b590e9a4ea4e911c43af4c150471aca4
AdrianOlivares77 <adrian.olivares.f.52@gmail.com>
2021-12-26 15:12:33 -0300

    feat(app): routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllNewsComponent } from './components/all-news/all-news.component';
+import { FavNewsComponent } from './components/fav-news/fav-news.component';
+import { HomeComponent } from './screens/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'all-news', component: AllNewsComponent},
+      { path: 'fav-news', component: FavNewsComponent}
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
